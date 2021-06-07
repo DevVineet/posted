79ccdea356bc7bc5488a504d3679f088dc5b6a00
Farhan <farhan@Farhans-MacBook-Air.local>
2021-06-07 13:20:12 +0530

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Posted</h1>
+      <p>Lets you post something to a global feed. Other users can react on those posts.</p>
+    </div>
+  );
+}
+
+export default App;
