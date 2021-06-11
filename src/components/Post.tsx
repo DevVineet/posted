449f2ab50bc738ec2a130c0dcfeb1f750439375a
Farhan <farhan2056@gmail.com>
2021-06-11 21:21:49 +0530

    image height fixed

--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -17,14 +17,14 @@ export const Post = ({
   date,
   keyword = "cars",
 }: Post) => (
-  <div className="m-2 rounded-md overflow-hidden">
-    {/* {num % 3 === 0 && (
+  <div className="m-2 overflow-hidden border-b border-dashed border-gray-300">
+    {num % 3 === 0 && (
       <img
-        className="w-full h-64 bg-gray-200"
+        className="w-full bg-gray-200"
         src={`https://source.unsplash.com/random/600x${400 + num}?${keyword}`}
         alt=""
       />
-    )} */}
+    )}
     <div className="p-4">
       <p className="text-md py-2">{content}</p>
       <div className="text-gray-600 flex text-xs justify-between">
